5ea362b3dc6617c7841406b8e27fc5da2f4142e7
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-12 12:28:20 -0400

    Add JSON extensions

---
+++ Sources/VideoStreaming/Extensions/JSON.swift
@@ -0,0 +1,16 @@
+//
+//  JSON.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/12/23.
+//
+
+import Foundation
+
+public extension JSONDecoder {
+    
+    static var videoStreaming: JSONDecoder {
+        let decoder = JSONDecoder()
+        return decoder
+    }
+}
